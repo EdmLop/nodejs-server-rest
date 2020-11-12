ebd8f06849a9cdcebc8e7f4a33cb6f5048352a0c
Edmundo <correoedmundo@gmail.com>
2020-11-12 17:45:24 -0600

    Implementacion de servicios REST y base de datos en la nube

--- server/config/config.js
+++ server/config/config.js
@@ -4,3 +4,24 @@ Puerto
 */
 
 process.env.PORT = process.env.PORT || 3000;
+
+/*=================================
+Entorno
+=================================
+*/
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+/*=================================
+DB
+=================================
+*/
+let urldb;
+
+if (process.env.NODE_ENV === 'dev') {
+    urldb = 'mongodb://localhost:27017/cafe';
+} else {
+    urldb = 'mongodb+srv://edmundolopez:w5sDhXprxmUAipuD@cluster0.fv69p.mongodb.net/cafe';
+}
+
+process.env.URLDB = urldb;
