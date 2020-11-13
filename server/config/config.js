8119d6a1b4348e01ad43e13ed10839686c98d141
Edmundo <correoedmundo@gmail.com>
2020-11-12 20:21:49 -0600

    Modificación de url a variable de entorno heroku

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urldb;
 if (process.env.NODE_ENV === 'dev') {
     urldb = 'mongodb://localhost:27017/cafe';
 } else {
-    urldb = MONGO_URI;
+    urldb = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urldb;
